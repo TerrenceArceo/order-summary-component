f81595e2658bd656d0e50266eb4739373cee6b01
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2021-12-28 12:59:45 -0800

    Added buttons and styling. Adjusted margins for breathing room as well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Plan from './components/Plan'
+import PaymentButtons from './components/PaymentButtons';
 
 function App() {
   return (
     <div className='container'>
       <Header />
       <Plan />
+      <PaymentButtons />
     </div>
   );
 }
---
+++ src/components/PaymentButtons.js
@@ -0,0 +1,8 @@
+export default function PaymentButtons() {
+    return (
+        <div className="payment-buttons">
+            <button className="btn proceed">Proceed to Payment</button>
+            <button className="cancel">Cancel Order</button>
+        </div>
+    )
+}
